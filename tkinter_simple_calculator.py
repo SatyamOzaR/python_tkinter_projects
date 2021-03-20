91d46e0356389f494a2bc094d42d0431760914d6
SatyamOzaR <satyamozar@gmail.com>
2021-03-20 16:58:48 +0530

    initial

--- tkinter_simple_calculator.py
+++ tkinter_simple_calculator.py
@@ -108,4 +108,5 @@ if __name__ == "__main__":
                     command=lambda: press('.'), height=1, width=7) 
     Decimal.grid(row=6, column=0) 
     # start the GUI 
-    gui.mainloop() 
+    gui.mainloop()
+    #verified 
